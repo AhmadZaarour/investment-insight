d0f2dde4d5d688e7aec0f53f83118c6b69485b47
Ahmad Zaarour <ahmdzarour430@gmail.com>
2025-04-14 18:02:25 +0300

    added api limiters

--- models/fetch_news.py
+++ models/fetch_news.py
@@ -1,15 +1,24 @@
 import requests
 import os
 from dotenv import load_dotenv
+from ratelimit import limits, sleep_and_retry
 
 load_dotenv()
+
+@sleep_and_retry
+@limits(calls=60, period=60)
+def call_api(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception("API response: {}".format(response.status_code))
+    return response.json()
 
 def fetch_news():
     api_key = os.getenv("API_KEY")
     url = f"https://newsapi.org/v2/everything?q=finance&apiKey={api_key}"
 
-    response = requests.get(url)
-    data = response.json()
+    response = call_api(url)
+    data = response
 
     articles = []
 
